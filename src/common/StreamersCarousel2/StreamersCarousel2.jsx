2189c9a6217d9fd68a75c6c66927fff1f97d23cf
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-12-15 16:40:33 +0100

    fix:carousel2 to aws

--- src/common/StreamersCarousel2/StreamersCarousel2.jsx
+++ src/common/StreamersCarousel2/StreamersCarousel2.jsx
@@ -1,29 +1,37 @@
 import "./StreamersCarousel2.css";
+import streamer1 from "../../assets/images/WEBIMAGES/streamer1.jpg";
+import streamer2 from "../../assets/images/WEBIMAGES/streamer2.jpg";
+import streamer3 from "../../assets/images/WEBIMAGES/streamer3.jpg";
+import streamer4 from "../../assets/images/WEBIMAGES/streamer4.jpg";
+import streamer5 from "../../assets/images/WEBIMAGES/streamer5.jpg";
+import streamer6 from "../../assets/images/WEBIMAGES/streamer6.jpg";
+import streamer7 from "../../assets/images/WEBIMAGES/streamer7.jpg";
+import streamer8 from "../../assets/images/WEBIMAGES/streamer8.jpg";
 
 const StreamersCarousel2 = () => {
   return (
     <div>
       <div className="logos2">
         <div className="logos2-slide-left">
-          <img src="../src/assets/images/WEBIMAGES/streamer1.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer2.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer3.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer4.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer5.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer6.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer7.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer8.jpg" />
+          <img src={streamer1} />
+          <img src={streamer2} />
+          <img src={streamer3} />
+          <img src={streamer4} />
+          <img src={streamer5} />
+          <img src={streamer6} />
+          <img src={streamer7} />
+          <img src={streamer8} />
         </div>
 
         <div className="logos2-slide-left">
-          <img src="../src/assets/images/WEBIMAGES/streamer1.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer2.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer3.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer4.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer5.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer6.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer7.jpg" />
-          <img src="../src/assets/images/WEBIMAGES/streamer8.jpg" />
+          <img src={streamer1} />
+          <img src={streamer2} />
+          <img src={streamer3} />
+          <img src={streamer4} />
+          <img src={streamer5} />
+          <img src={streamer6} />
+          <img src={streamer7} />
+          <img src={streamer8} />
         </div>
       </div>
     </div>
